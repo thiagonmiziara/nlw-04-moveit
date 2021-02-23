0682a19824f0815ef69ae3295916e0fea7c1f674
thiagonmiziara <thiagonmiziara@gmail.com>
2021-02-23 19:24:22 -0300

    add componets completChallenges / Countdown

---
+++ moveit-next/src/components/CompletedChallenges.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "../styles/components/CompletedChallenges.module.css";
+
+const CompletedChallenges = () => {
+  return (
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios Completos</span>
+      <span>05</span>
+    </div>
+  );
+};
+
+export default CompletedChallenges;
---
+++ moveit-next/src/components/Countdown.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "../styles/components/Countdown.module.css";
+
+const Countdown = () => {
+  return (
+    <div className={styles.countdownContainer}>
+      <div>
+        <span>2</span>
+        <span>5</span>
+      </div>
+      <span>:</span>
+      <div>
+        <span>0</span>
+        <span>0</span>
+      </div>
+    </div>
+  );
+};
+
+export default Countdown;
